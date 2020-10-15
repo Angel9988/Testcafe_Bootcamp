f6e009fe28b54c3d060a2e8262a5026753d286b4
Angel9988 <mega_1287@hotmail.com>
2020-10-15 13:59:54 -0500

    Variables Javascripts

---
+++ constante.js
@@ -0,0 +1,9 @@
+//Constantes
+
+const nombre = 'Angel'
+
+console.log(nombre)
+
+nombre = 'ajsdjlpajl'
+
+console.log(nombre)
---
+++ variables.js
@@ -0,0 +1,9 @@
+//Variables
+
+let edad = 40
+
+console.log(edad)
+
+edad = "Hola TestCafe"
+
+console.log(edad)
